6694d0eb6aa70edfe9b60352fa5ba4395c443578
Filip Danić <filip@spicefactory.co>
2018-03-26 14:52:53 +0200

    correct publish mistake + add test for senderId

--- src/tests/listify.test.js
+++ src/tests/listify.test.js
@@ -7,6 +7,9 @@ const test = {
     {
       messaging: [
         {
+          sender: {
+            id: 1000,
+          },
           message: {
             text: 'hello, world!',
             quick_reply: {
@@ -15,11 +18,17 @@ const test = {
           }
         },
         {
+          sender: {
+            id: 2000,
+          },
           message: {
             attachments: [{ val: 'FOO2' }]
           },
         },
         {
+          sender: {
+            id: 3000,
+          },
           message: {
             is_echo: true
           }
@@ -29,11 +38,17 @@ const test = {
     {
       messaging: [
         {
+          sender: {
+            id: 4000,
+          },
           postback: {
             payload: 'FOO3'
           }
         },
         {
+          sender: {
+            id: 5000,
+          },
           read: {
             watermark: 1458668856253
           }
@@ -51,4 +66,10 @@ console.assert(result[1].type === EVENT_TYPES.ATTACHMENTS);
 console.assert(result[2].type === EVENT_TYPES.ECHO);
 console.assert(result[3].type === EVENT_TYPES.POSTBACK);
 console.assert(result[4].type === EVENT_TYPES.READ);
+
+console.assert(result[0].senderId === 1000);
+console.assert(result[1].senderId === 2000);
+console.assert(result[2].senderId === 3000);
+console.assert(result[3].senderId === 4000);
+console.assert(result[4].senderId === 5000);
 console.log('✅ all tests are passing.');
